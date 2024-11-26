cfb3da3defa55b81c4a0647b6c85827c39068f2c
LailyFauziah23 <lailynuriyatulfauziah@mail.ugm.ac.id>
2024-11-26 22:47:55 +0700

    tambahan

--- app/src/main/java/com/example/sharedpreference/MainActivity.kt
+++ app/src/main/java/com/example/sharedpreference/MainActivity.kt
@@ -2,87 +2,115 @@ package com.example.sharedpreference
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.sharedpreference.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private val binding by lazy {
-        ActivityMainBinding.inflate(layoutInflater)
-    }
 
+    private lateinit var binding: ActivityMainBinding
     private lateinit var prefManager: PrefManager
-    private val chanelID = "TES_NOTIF"
+
+    // Untuk nilai channelId dan notifId
+    private val channelId = "TEST_NOTIF"
+    private val notifId = 90
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         prefManager = PrefManager.getInstance(this)
         checkLoginStatus()
+
         with(binding) {
             btnLogin.setOnClickListener {
                 val usernameUntukLogin = "admin"
                 val passwordUntukLogin = "12345"
+
                 val username = edtUsername.text.toString()
                 val password = edtPassword.text.toString()
-                if (username.isEmpty() || password.isNotEmpty()) {
-                    Toast.makeText(this@MainActivity, "Username dan password harus diisi",
-                        Toast.LENGTH_SHORT).show()
-                }else if (username.equals("usernameUntukLogin") &&
-                    password.equals("passwordUntukLogin")){
-                    prefManager.saveUsername(username)
-                    checkLoginStatus()
-                }else{
-                    Toast.makeText(this@MainActivity, "Username atau password salah",
-                        Toast.LENGTH_SHORT).show()
+
+                when {
+                    username.isEmpty() || password.isEmpty() -> {
+                        Toast.makeText(
+                            this@MainActivity,
+                            "Username dan password harus diisi",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                    username == usernameUntukLogin && password == passwordUntukLogin -> {
+                        prefManager.saveUsername(username)
+                        checkLoginStatus()
+                        Toast.makeText(this@MainActivity, "Login berhasil", Toast.LENGTH_SHORT).show()
+                    }
+                    else -> {
+                        Toast.makeText(
+                            this@MainActivity,
+                            "Username atau password salah",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
                 }
             }
+
             btnLogout.setOnClickListener {
                 prefManager.clearUsername()
                 checkLoginStatus()
+                Toast.makeText(this@MainActivity, "Logout berhasil", Toast.LENGTH_SHORT).show()
             }
-            btnClear.setOnClickListener {
-                prefManager.clearUsername()
-                checkLoginStatus()
-            }
-            btnNotif.setOnClickListener {
-                val builder = NotificationCompat.Builder(this@MainActivity, "my_channel")
-                    .setSmallIcon(R.drawable.baseline_notifications_24)
-                    .setContentTitle("Notifikasi PPPB")
-                    .setContentText("Hello from the other side")
-                    .setAutoCancel(true)
 
-                val notificationManager= getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                    val notificationChannel=NotificationChannel(
-                        chanelID,
-                        "Notif PPPB",
-                        NotificationManager.IMPORTANCE_DEFAULT
-                    )
-                    with(notificationManager){
-                        createNotificationChannel(notificationChannel)
-                        notify(0, builder.build())
-                    }
-                } else{
-                    notificationManager.notify(0, builder.build())
-                }
+            btnNotif.setOnClickListener {
+                showNotification()
             }
         }
     }
-    fun checkLoginStatus() {
+
+    private fun checkLoginStatus() {
         val isLoggedIn = prefManager.getUsername()
         if (isLoggedIn.isEmpty()) {
-            binding.llLogged.visibility = View.VISIBLE
-            binding.llLogin.visibility = View.GONE
-        } else {
-            binding.llLogged.visibility = View.GONE
             binding.llLogin.visibility = View.VISIBLE
+            binding.llLogged.visibility = View.GONE
+        } else {
+            binding.llLogin.visibility = View.GONE
+            binding.llLogged.visibility = View.VISIBLE
         }
+    }
+
+    private fun showNotification() {
+        val notifManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val flag = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            PendingIntent.FLAG_IMMUTABLE
+        } else {
+            0
+        }
+
+        val intent = Intent(this, NotifReceiver::class.java).putExtra("MESSAGE", "Baca selengkapnya ...")
+        val pendingIntent = PendingIntent.getBroadcast(this, 0, intent, flag)
+
+        val builder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.baseline_notifications_24)
+            .setContentTitle("Notifikasi PPPB")
+            .setContentText("Hello World!")
+            .setAutoCancel(true)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .addAction(0, "Baca Notif", pendingIntent)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notifChannel = NotificationChannel(
+                channelId,
+                "Notifku",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            notifManager.createNotificationChannel(notifChannel)
+        }
+        notifManager.notify(notifId, builder.build())
     }
 }
